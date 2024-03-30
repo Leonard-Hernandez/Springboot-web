acb9366c27419fec9bcb70906c45b49889900b93
Leonard <Leonardhernandez02@gmail.com>
2024-03-30 15:01:21 -0500

    implementando dto

--- src/main/java/com/leonard/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/leonard/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -8,19 +8,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.leonard.curso.springboot.webapp.springboot_web.models.User;
+import com.leonard.curso.springboot.webapp.springboot_web.models.dto.UserDto;
 
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
 
     @GetMapping("/details")
-    public  Map<String,Object> details(){
+    public  UserDto details(){
+        UserDto userDto  = new UserDto();
         User user = new User("Leonard", "Hernandez");
+        userDto.setUser(user);
+        userDto.setTitle("Hola Mundo Spring Boot");
         
-        Map<String,Object> body = new HashMap<>();
-        body.put("title", "Hola Mundo Spring Boot");
-        body.put("user", user); 
-        return body;
+ 
+        return userDto;
     }    
   
 
