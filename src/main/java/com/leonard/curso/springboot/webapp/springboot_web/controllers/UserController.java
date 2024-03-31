36f5e7218312f8c4b6baba678eb1d6caaa54aa82
Leonard <Leonardhernandez02@gmail.com>
2024-03-31 17:15:58 -0500

    preparando el list para thymeleaf

--- src/main/java/com/leonard/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/leonard/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,7 +1,11 @@
 package com.leonard.curso.springboot.webapp.springboot_web.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.leonard.curso.springboot.webapp.springboot_web.models.User;
@@ -16,5 +20,14 @@ public class UserController {
         model.addAttribute("title", "Hola Mundo Spring Boot");
         model.addAttribute("user", user);        
         return "details";
+    }
+
+    @GetMapping("/list")
+    public String list(ModelMap model){
+        List<User> users = new ArrayList<>();
+        
+        model.addAttribute("title", "Lista de usuarios");
+        model.addAttribute("users", users);
+        return "list";
     }
 }
