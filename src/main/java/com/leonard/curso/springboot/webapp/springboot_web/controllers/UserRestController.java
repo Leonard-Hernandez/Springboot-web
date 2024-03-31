70b6b92189e8d21dee463708f03ca3de90a55a93
Leonard <Leonardhernandez02@gmail.com>
2024-03-31 16:43:30 -0500

    Agregando el metodo listar

--- src/main/java/com/leonard/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/leonard/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -1,7 +1,8 @@
 package com.leonard.curso.springboot.webapp.springboot_web.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.leonard.curso.springboot.webapp.springboot_web.models.User;
 import com.leonard.curso.springboot.webapp.springboot_web.models.dto.UserDto;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/api")
@@ -23,7 +26,24 @@ public class UserRestController {
         
  
         return userDto;
-    }    
+    }
+
+    
+    @GetMapping("list")    
+    public List<User> list(){
+        User leonard = new User("Leonard", "Hernandez");
+        User lubraska = new User("Lubraska", "Ferreira");
+        User andres= new User("Andres", "Guzman");
+
+        List<User> users = Arrays.asList(leonard, lubraska, andres);
+
+        // List<User> users = new ArrayList<>();
+        // users.add(leonard);
+        // users.add(lubraska);
+        // users.add(andres);
+
+        return users;
+    }
   
 
 
