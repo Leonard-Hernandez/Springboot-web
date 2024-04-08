6718d536be45589b7c43779166a95c5eaa5c1f6b
Leonard <Leonardhernandez02@gmail.com>
2024-04-07 19:21:20 -0500

    Usando Path Variable en springboot

--- src/main/java/com/leonard/curso/springboot/webapp/springboot_web/controllers/RequesParamsController.java
+++ src/main/java/com/leonard/curso/springboot/webapp/springboot_web/controllers/RequesParamsController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.leonard.curso.springboot.webapp.springboot_web.models.dto.ParamDto;
 import com.leonard.curso.springboot.webapp.springboot_web.models.dto.ParamMixDto;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/api/params")
 public class RequesParamsController {
@@ -28,6 +30,18 @@ public class RequesParamsController {
         return params;
     }
 
-    
+    @GetMapping("/request")
+    public ParamMixDto request(HttpServletRequest request){
+        Integer code = 0;
+        try {
+            code = Integer.parseInt(request.getParameter("code"));
+        } catch (NumberFormatException e) {            
+        }
+        
+        ParamMixDto params = new ParamMixDto();
+        params.setCode(code);
+        params.setMessage(request.getParameter("message"));
+        return params;
+    }
 
 }
--- src/main/java/com/leonard/curso/springboot/webapp/springboot_web/models/dto/ParamDto.java
+++ src/main/java/com/leonard/curso/springboot/webapp/springboot_web/models/dto/ParamDto.java
@@ -3,7 +3,7 @@ package com.leonard.curso.springboot.webapp.springboot_web.models.dto;
 public class ParamDto {
 
     private String message;
-    private Integer code;
+    
     public String getMessage() {
         return message;
     }
