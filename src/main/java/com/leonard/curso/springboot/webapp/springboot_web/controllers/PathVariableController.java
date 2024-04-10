a6c7b96a9d2fd4b3b64b015982dc40ebd4cee6b0
Leonard <Leonardhernandez02@gmail.com>
2024-04-09 20:45:06 -0500

    recibiendo un json en un endpoint

--- src/main/java/com/leonard/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/leonard/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -5,9 +5,12 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.leonard.curso.springboot.webapp.springboot_web.models.User;
 import com.leonard.curso.springboot.webapp.springboot_web.models.dto.ParamDto;
 
 @RestController
@@ -29,6 +32,13 @@ public class PathVariableController {
         json.put("product", product);
         json.put("id", id);
         return json;
+    }
+
+    @PostMapping("/create")
+    public User create(@RequestBody User user){
+        //hacer algo como un save en la base de datos
+        user.setName(user.getName().toUpperCase());
+        return user;
     }
 
 
